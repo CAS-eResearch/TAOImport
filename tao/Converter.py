b72013fa4a1a55624e1c518dae18f535f413f905
Manodeep Sinha <manodeep@gmail.com>
2015-10-26 13:54:46 +1100

    Moved the output of xml file before processing the data. Added section timings for every chunk of code in convert_tree

--- tao/Converter.py
+++ tao/Converter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from .library import library
 from .Exporter import Exporter
 from .Mapping import Mapping
@@ -75,6 +76,9 @@ class Converter(object):
         library['redshifts'] = redshifts
         library['n_snapshots'] = len(redshifts)
 
+        with open('settings.xml', 'w') as file:
+            file.write(get_settings_xml(self.galaxy_type))
+        
         with Exporter(self.args.output, self) as exp:
             exp.set_cosmology(sim['hubble'], sim['omega_m'], sim['omega_l'])
             exp.set_box_size(sim['box_size'])
@@ -82,71 +86,64 @@ class Converter(object):
             for tree in self.iterate_trees():
                 exp.add_tree(tree)
 
-        with open('settings.xml', 'w') as file:
-            file.write(get_settings_xml(self.galaxy_type))
-
     def convert_tree(self, src_tree):
-        dst_tree = np.empty(len(src_tree), self.galaxy_type)
-
+        tstart = time.time()
+        dst_tree = np.empty_like(src_tree, dtype = self.galaxy_type)
+        t0 = time.time()
         # Do conversion first.
         fields = {}
         for mod in self.modules:
             mod.convert_tree(src_tree, fields)
 
+        mod_time = time.time() - t0    
+
+        t0 = time.time()
         # Next check for any issues.
         for mod in self.modules:
             mod.validate_fields(fields)
 
+        val_time = time.time() - t0
+
+        
+        t0 = time.time()
         # Now perform generation.
         for mod in self.modules:
             mod.generate_fields(fields)
 
+        gen_time = time.time() - t0
+            
+        t0 = time.time()
         # Now we can merge the fields into the tree.
         for name, values in fields.iteritems():
             x = dst_tree[name]
             x[:] = values
 
+        iter_time=time.time() - t0
+
+        t0 = time.time() 
         # Perform a direct transfer of fields from within the
         # mapping that are flagged.
         for field, dtype in self.mapping.fields:
             dst_tree[field] = src_tree[field]
 
+        copy_time = time.time() - t0
+
+        t0 = time.time()
         # Now run any post conversion routines.
         for mod in self.modules:
             mod.post_conversion(dst_tree)
+            
+        post_conv_time = time.time() - t0
+        total_time = time.time() - tstart
+
+        print "{:10.6f} {:10.6f}({:4.1f}%)  {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%)".format(total_time,
+                                                                                                                                      mod_time,mod_time/total_time*100.0,
+                                                                                                                                      val_time,val_time/total_time*100.0,
+                                                                                                                                      gen_time,gen_time/total_time*100.0,
+                                                                                                                                      iter_time,iter_time/total_time*100.0,
+                                                                                                                                      copy_time,copy_time/total_time*100.0,
+                                                                                                                                      post_conv_time,post_conv_time/total_time*100.0
+                                                                                                                                      )
 
         return dst_tree
 
-    # def depth_first_order(self, tree):
-    #     dfi = [0]
-    #     parents = {}
-    #     order = np.empty(len(tree))
-
-    #     def _recurse(idx):
-    #         order[idx] = dfi[0]
-    #         dfi[0] += 1
-    #         tree['subsize'][idx] = 1
-    #         if idx in parents:
-    #             for par in parents[idx]:
-    #                 tree['subsize'][idx] += _recurse(par)
-    #         return tree['subsize'][idx]
-
-    #     # Find the roots and parents.
-    #     roots = []
-    #     for ii in range(len(tree)):
-    #         desc = tree['descendant'][ii]
-    #         if desc == -1:
-    #             roots.append(ii)
-    #         else:
-    #             parents.setdefault(desc, []).append(ii)
-
-    #     # Recurse to find the new ordering.
-    #     for ii in roots:
-    #         _recurse(ii)
-
-    #     # Remap everything.
-    #     for ii in range(len(tree)):
-    #         tree['local_index'][ii] = order[tree['local_index'][ii]]
-    #         if tree['descendant'][ii] != -1:
-    #             tree['descendant'][ii] = order[tree['descendant'][ii]]
-    #             tree['global_descendant'][ii] = tree['global_index'][tree['descendant'][ii]]
