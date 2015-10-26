fa41f770c66591b511a9d4aecaabd2fe4b6b98d5
Manodeep Sinha <manodeep@gmail.com>
2015-10-26 13:58:28 +1100

    Changed snapshot and type to signed integers

--- tao/datatype.py
+++ tao/datatype.py
@@ -2,8 +2,8 @@ import h5py
 import numpy as np
 
 datatype = np.dtype([
-    ('snapshot', np.uint32),
-    ('type',     np.uint32),
+    ('snapshot', np.int32),
+    ('type',     np.int32),
     ('position_x', np.float32), ('position_y', np.float32), ('position_z', np.float32),
     ('velocity_x', np.float32), ('velocity_y', np.float32), ('velocity_z', np.float32),
 ])
