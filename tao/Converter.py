Fixed off-by-one in globalindex. Added white-space to silence flymake. Validation added for descendants and global descendants

tao/Converter.py
@@ -42,7 +42,7 @@ class Converter(object):
             updated_dict[key] = new_dict[key]
 
         return updated_dict
-            
+
     def make_datatype(self):
         all_fields = []
         seen_fields = set()
@@ -51,7 +51,6 @@ class Converter(object):
         pp = pprint.PrettyPrinter(indent=4)
 
         # print "BEGINNING self.mapping.fields = {0}".format(self.mapping.fields)
-        
         for mod in self.modules:
             # print "inside converter make_dataypes - mod = {0}".format(mod)
             new_fields, meta_field = mod.get_numpy_fields()
@@ -71,10 +70,9 @@ class Converter(object):
                     #     .format(mod, nf[0])
 
                     # print "old dict = {0}\nnew_dict = {1}".format(old_d, new_d)
-                    
                     updated_d = self.combine_and_append_keys(old_d, new_d)
                     metadata[lower_case_field] = updated_d
-                    
+
         mapped_keys_lower = [k.lower() for k in self.mapping.table.keys()]
         for mf, d in self.mapping.fields.iteritems():
             lower_case_field = mf.lower()
@@ -83,7 +81,7 @@ class Converter(object):
                 "of fields to directly transfer. Please remove this field "\
                 "from either get_mapping_table() or get_extra_fields(). "\
                 .format(lower_case_field)
-            
+
             if lower_case_field not in seen_fields:
                 all_fields.append((mf, d['type']))
                 seen_fields.add(lower_case_field)
@@ -116,10 +114,8 @@ class Converter(object):
             #     .format(key, val)
 
             # print "old dict = {0}\nnew_dict = {1}".format(old_d, new_d)
-            
             updated_d = self.combine_and_append_keys(old_d, new_d)
             metadata[lower_case_field] = updated_d
-                            
 
         # print "all_fields = {0}".format(all_fields)
         try:
@@ -175,19 +171,22 @@ class Converter(object):
         library['hubble'] = sim['hubble']
 
         if not self.args.sim_name:
-            raise ConversionError('Must specify "sim-name": name for the Simulation (dark matter/hydro)')
+            msg = 'Must specify "sim-name": name for the Simulation '\
+                '(dark matter/hydro)'
+            raise ConversionError(msg)
         library['sim-name']  = self.args.sim_name
 
         if not self.args.model_name:
-            raise ConversionError('Must specify a "model-name": name for the SAM galaxy formation model (use simulation name for hydro)')
+            msg = 'Must specify a "model-name": name for the SAM galaxy '\
+                'formation model (use simulation name for hydro)'
+            raise ConversionError(msg)
 
         library['model-name'] = self.args.model_name
 
-        
-        with open(os.path.dirname(self.args.output)+'/settings.xml', 'w') as f:
+        outfilename = os.path.dirname(self.args.output) + '/settings.xml'
+        with open(outfilename, 'w') as f:
             f.write(get_settings_xml(self.galaxy_type, redshifts,
                                      self.metadata))
-        
         with Exporter(self.args.output, self) as exp:
             exp.set_cosmology(sim['hubble'], sim['omega_m'], sim['omega_l'])
             exp.set_box_size(sim['box_size'])
@@ -197,14 +196,15 @@ class Converter(object):
 
     def convert_tree(self, src_tree):
         tstart = time.time()
-        dst_tree = np.empty_like(src_tree, dtype = self.galaxy_type)
+        dst_tree = np.empty_like(src_tree,
+                                 dtype=self.galaxy_type)
         t0 = time.time()
         # Do conversion first.
         fields = {}
         for mod in self.modules:
             mod.convert_tree(src_tree, fields)
 
-        mod_time = time.time() - t0    
+        mod_time = time.time() - t0
 
         t0 = time.time()
         # Next check for any issues.
@@ -213,21 +213,18 @@ class Converter(object):
 
         val_time = time.time() - t0
 
-        
         t0 = time.time()
         # Now perform generation.
         for mod in self.modules:
             mod.generate_fields(fields)
 
         gen_time = time.time() - t0
-            
         t0 = time.time()
         # Now we can merge the fields into the tree.
         self._merge_fields(fields, dst_tree)
+        # iter_time=time.time() - t0
 
-        iter_time=time.time() - t0
-
-        t0 = time.time() 
+        # t0 = time.time()
         # Perform a direct transfer of fields from within the
         # mapping that are flagged.
         self._transfer_fields(src_tree, dst_tree)
@@ -238,7 +235,7 @@ class Converter(object):
         # Now run any post conversion routines.
         for mod in self.modules:
             mod.post_conversion(dst_tree)
-            
+
         post_conv_time = time.time() - t0
         total_time = time.time() - tstart
 
@@ -265,4 +262,3 @@ class Converter(object):
         # print "dst_tree.dtype = {0}".format(dst_tree.dtype)
         for field, _ in self.mapping.fields.iteritems():
             dst_tree[field] = src_tree[field]
-

tao/Exporter.py
@@ -1,7 +1,10 @@
-import h5py, logging, numpy as np
+import h5py
+import logging
+import numpy as np
 from LightCone import LightCone
 
 logger = logging.getLogger(__name__)
+
 
 class Exporter(object):
 
@@ -13,12 +16,12 @@ class Exporter(object):
     def open_file(self, filename):
         self.file = h5py.File(filename, 'w')
         self.tree_counts = self.file.create_dataset(
-            'tree_counts', (0,), dtype='i',
+            'tree_counts', (0,), dtype='uint32',
             chunks=(self.chunk_size,),
             maxshape=(None,)
         )
         self.tree_displs = self.file.create_dataset(
-            'tree_displs', (1,), dtype='i',
+            'tree_displs', (1,), dtype='uint64',
             chunks=(self.chunk_size,),
             maxshape=(None,)
         )
@@ -34,10 +37,13 @@ class Exporter(object):
             maxshape=(None,)
         )
         self.cosmology = self.file.create_group('cosmology')
-        self.box_size = self.cosmology.create_dataset('box_size', (1,), dtype='f')
+        self.box_size = self.cosmology.create_dataset('box_size',
+                                                      (1,), dtype='f')
         self.hubble = self.cosmology.create_dataset('hubble', (1,), dtype='f')
-        self.omega_m = self.cosmology.create_dataset('omega_m', (1,), dtype='f')
-        self.omega_l = self.cosmology.create_dataset('omega_l', (1,), dtype='f')
+        self.omega_m = self.cosmology.create_dataset('omega_m', (1,),
+                                                     dtype='f')
+        self.omega_l = self.cosmology.create_dataset('omega_l', (1,),
+                                                     dtype='f')
 
     def __enter__(self):
         return self
@@ -46,7 +52,7 @@ class Exporter(object):
         self.file.close()
 
     def add_tree(self, tree):
-        cnt = len(tree)
+        cnt = np.uint32(len(tree))
         displ = self.tree_displs[-1]
         self.tree_counts.resize((self.tree_counts.shape[0] + 1,))
         self.tree_displs.resize((self.tree_displs.shape[0] + 1,))

tao/generators.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 import numpy as np
 import time as time
 import sys
-from IPython.core.debugger import Tracer
+
 
 class Generator(object):
 
@@ -27,24 +27,26 @@ class Generator(object):
         pass
 
 class GlobalIndices(Generator):
-    fields = [
-        ('globalindex', np.int64)
-        ]
-    
+    fields = [('globalindex', np.int64)]
+
     def __init__(self, *args, **kwargs):
         super(GlobalIndices, self).__init__(*args, **kwargs)
         self.index = 0
 
     def generate_fields(self, fields):
         gidxs = self.get_field(fields, 'globalindex', np.int64)
-        gidxs[:] = np.arange(self.index, self.index+len(gidxs),1,dtype=np.int64)
-        self.index += len(gidxs)-1
+        gidxs[:] = np.arange(self.index, self.index + len(gidxs),
+                             1, dtype=np.int64)
+        self.index += len(gidxs)
+        # gidxs = self.get_field(fields, 'globalindex', np.int64)
+        # for ii in range(len(gidxs)):
+        #     gidxs[ii] = self.index + ii
+        # self.index += len(gidxs)
+
 
 class TreeIndices(Generator):
-    fields = [
-        ('treeindex', np.int32)
-        ]
-    
+    fields = [('treeindex', np.int32)]
+
     def __init__(self, *args, **kwargs):
         super(TreeIndices, self).__init__(*args, **kwargs)
         self.index = 0
@@ -54,39 +56,34 @@ class TreeIndices(Generator):
         tidxs[:] = self.index
         self.index += 1
 
+
 class TreeLocalIndices(Generator):
-    fields = [
-        ('localindex', np.int32)
-        ]
+    fields = [('localindex', np.int32)]
 
     def generate_fields(self, fields):
         lidxs = self.get_field(fields, 'localindex', np.int32)
-        lidxs[:] = np.arange(0,len(lidxs),1,dtype=np.int32)
+        lidxs[:] = np.arange(0, len(lidxs), 1, dtype=np.int32)
         # print("lidxs = {0} shape = {1}".format(lidxs, lidxs.shape))
-                
-            
+        # for ii in range(len(lidxs)):
+        #     lidxs[ii] = ii
+
+
 class GlobalDescendants(Generator):
-    fields =[
-        ('globaldescendant', np.int64)
-        ]
+    fields = [('globaldescendant', np.int64)]
 
     def generate_fields(self, fields):
         gdescs = self.get_field(fields, 'globaldescendant', np.int64)
         descs = fields['descendant']
-        # print("len(gdescs) = {0} len(descs) = {1}"\
-        #           .format(len(gdescs), len(descs)))
         gidxs = fields['globalindex']
 
-        # Tracer()()
         gdescs[:] = descs
         ind = (np.where(descs != -1))[0]
         if len(ind) > 0:
             gdescs[ind] = gidxs[descs[ind]]
 
+
 class DepthFirstOrdering(Generator):
-    fields = [
-            ('subsize', np.int32)
-            ]
+    fields = [('subsize', np.int32)]
 
     def post_conversion(self, tree):
         tstart = time.time()
@@ -103,7 +100,6 @@ class DepthFirstOrdering(Generator):
                     tree['subsize'][idx] += _recurse(par)
             return tree['subsize'][idx]
 
-        
         # Find the roots and parents.
         t0 = time.time()
         ind = (np.where(tree['descendant'] == -1))[0]
@@ -112,17 +108,17 @@ class DepthFirstOrdering(Generator):
 
         ind = (np.where(tree['descendant'] != -1))[0]
         if len(ind) > 0:
-            for ii, desc in zip(ind,tree['descendant'][ind]):
-                parents.setdefault(desc,[]).append(ii)
-                
+            for ii, desc in zip(ind, tree['descendant'][ind]):
+                parents.setdefault(desc, []).append(ii)
+
         roots_time = time.time() - t0
 
-        ## used to time the previous recursive chunk.
-        ## now here so I don't break the code or file
-        ## output columns
+        # used to time the previous recursive chunk.
+        # now here so I don't break the code or file
+        # output columns
         recurse_time = 0.0
-        
-        ## find new ordering. avoid (slow) recursion
+
+        # find new ordering. avoid (slow) recursion
         t0 = time.time()
         start_dfi = [0]
         tree['subsize'] = 0
@@ -138,33 +134,36 @@ class DepthFirstOrdering(Generator):
                 if idx in parents:
                     for par in parents[idx]:
                         stack.append(par)
-            
+
         new_recurse_time = time.time() - t0
-        
+
         # Remap everything.
         t0 = time.time()
         tree['localindex'] = order[tree['localindex']]
         ind = (np.where(tree['descendant'] != -1))[0]
         if len(ind) > 0:
-            tree['descendant'][ind] = order[tree['descendant'][ind]]
-            tree['globaldescendant'][ind] =  tree['globalindex'][tree['descendant'][ind]]
-        
+            ind1 = tree['descendant'][ind]
+            tree['descendant'][ind] = order[ind1]
+            tree['globaldescendant'][ind] = tree['globalindex'][ind1]
+
         remapping_time = time.time() - t0
 
         # Sort the array.
         t0 = time.time()
         tree.sort(order=['localindex'])
         sort_time = time.time() - t0
-        
+
         # Run some final checks on the descendants.
         t0 = time.time()
-        assert np.max(tree['descendant']) < len(tree),"Invalid descendant index."
+        assert np.max(tree['descendant']) < len(tree), \
+            "Invalid descendant index."
         ind = (np.where(tree['descendant'] != -1))[0]
         if len(ind) > 0:
-            diff = set(tree['descendant'][ind] - ind) 
-            assert  0 not in diff,"Descendant references same object."
+            diff = set(tree['descendant'][ind] - ind)
+            assert 0 not in diff, "Descendant references same object."
 
-        ind = (np.where((tree['mergeIntoID'] == -1) & (tree['descendant'] != -1)))[0]
+        ind = (np.where((tree['mergeIntoID'] == -1) & (tree['descendant']
+                                                       != -1)))[0]
         if len(ind) > 0:
             desc_ind = tree['descendant'][ind]
             desc_galidx = tree['GalaxyIndex'][desc_ind]
@@ -172,11 +171,27 @@ class DepthFirstOrdering(Generator):
             assert len(prog_galidx) == len(desc_galidx),\
                 "Bug in validation scheme for galaxy index"
             diff = np.unique(desc_galidx - prog_galidx)
-            assert  len(diff) == 1 and diff[0] == 0,\
+            assert len(diff) == 1 and diff[0] == 0,\
                 "Progenitor galaxy index must equal descendant galaxyindex. "\
                 "diff = {0}".format(diff)
-            
-                
+
+        # Validate globaldescendants
+        gdescs = tree['globaldescendant']
+        gidxs  = tree['globalindex']
+        descs  = tree['descendant']
+        for gdesc, desc in zip(gdescs, descs):
+            msg = "desc = {0} gdesc = {1}".format(desc, gdesc)
+            assert (((desc == -1) and (gdesc == -1)) or
+                    ((desc >= 0) and (gdesc >= 0))), msg
+
+            if desc == -1:
+                continue
+            msg = "gidxs[{0}] = {1} is not equal to gdesc = {2}".\
+                format(desc, gidxs[desc], gdesc)
+            if gidxs[desc] != gdesc:
+                Tracer()()
+            assert gidxs[desc] == gdesc, msg
+
         validation_time = time.time() - t0
         total_time = time.time() - tstart
         # print(" {:12d} {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%)  {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%) {:10.6f}({:4.1f}%)  {:10.6f}".format(len(tree),
@@ -187,3 +202,4 @@ class DepthFirstOrdering(Generator):
         #                                                                                                                                 recurse_time,recurse_time/total_time*100.0,
         #                                                                                                                                 new_recurse_time,new_recurse_time/total_time*100.0,
         #                                                                                                                                 total_time, file=sys.stderr))
+